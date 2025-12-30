b0a62faba882ffe80a56a771226792649df05af2
praisecancode <praiseeshemokhaio@gmail.com>
2025-12-31 02:32:52 +0800

    completed js DOM events and functions practice

--- mine.js
+++ mine.js
@@ -19,49 +19,75 @@
 // let getMyAge = getAge();
 // console.log("i am " + getMyAge + " years old")
 
-function getUsername(){
-    return "praise Eshemokhai! :)"
+// function getUsername(){
+//     return "praise Eshemokhai! :)"
+// }
+
+// // let name = getUsername();
+
+// document.getElementById("title").innerText = "dashboard";
+// document.getElementById("message").innerText = "Hello " + name;
+
+// function getItemCount(){
+//     return 3;
+// }
+
+// let itemsInCart = getItemCount(); {
+
+//     document.getElementById("cart").innerText = "items in your cart is " + itemsInCart;
+
+// }
+
+// function getUnReadMessages(){
+//     return 50;
+// }
+
+// let myUnreadMessages = getUnReadMessages(); {
+
+//     document.getElementById("unread").innerText = "you have " + myUnreadMessages + " unread messages";
+// }
+
+// function getAge(){
+//     return 18
+// }
+
+// let myAge = getAge(); {
+
+//     document.getElementById("age").innerText = "you are now " + getAge() + " years old";
+// }
+
+// function getNumberOfFriends() {
+//     return 2;
+// }
+
+// let numberOfFriends = getNumberOfFriends(); {
+
+//     document.getElementById("friends").innerText = "you have " + numberOfFriends + " new friends";
+// }
+
+let name = "praise";
+
+document.getElementById("username").textContent = name; 
+
+
+// alert("you are now connected as  " + name)
+
+let greeting = "hello welcome back!";
+
+document.getElementById("greeting").textContent = greeting
+
+// function and events
+
+function showMessage() {
+    document.getElementById("text").textContent = "welcome to my website";
 }
 
-let name = getUsername();
 
-document.getElementById("title").innerText = "dashboard";
-document.getElementById("message").innerText = "Hello " + name;
-
-function getItemCount(){
-    return 3;
+function clearMessage() {
+    document.getElementById("text").textcontent = "";
 }
 
-let itemsInCart = getItemCount(); {
+document.getElementById("btnShow").addEventListener("click", showMessage);
+document.getElementById("btnClear").addEventListener("click", clearMessage);
 
-    document.getElementById("cart").innerText = "items in your cart is " + itemsInCart;
-
-}
-
-function getUnReadMessages(){
-    return 50;
-}
-
-let myUnreadMessages = getUnReadMessages(); {
-
-    document.getElementById("unread").innerText = "you have " + myUnreadMessages + " unread messages";
-}
-
-function getAge(){
-    return 18
-}
-
-let myAge = getAge(); {
-
-    document.getElementById("age").innerText = "you are now " + getAge() + " years old";
-}
-
-function getNumberOfFriends() {
-    return 2;
-}
-
-let numberOfFriends = getNumberOfFriends(); {
-
-    document.getElementById("friends").innerText = "you have " + numberOfFriends + " new friends";
-}
 
