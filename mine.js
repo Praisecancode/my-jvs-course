9317d02aabb0357229cd74d5b96597776c4151fe
praisecancode <praiseeshemokhaio@gmail.com>
2026-03-10 11:39:20 +0100

    work on scopes, variables and hoisting

--- mine.js
+++ mine.js
@@ -65,46 +65,114 @@
 //     document.getElementById("friends").innerText = "you have " + numberOfFriends + " new friends";
 // }
 
-let name = "praise";
+//let name = "praise";
 
-document.getElementById("username").textContent = name; 
+//document.getElementById("username").textContent = name; 
 
 
 // alert("you are now connected as  " + name)
 
-let greeting = "hello welcome back!";
+//let greeting = "hello welcome back!";
 
-document.getElementById("greeting").textContent = greeting
+//document.getElementById("greeting").textContent = greeting
 
 // function and events
 
-function showMessage() {
-    document.getElementById("text").textContent = "welcome to my website";
-}
+//function showMessage() {
+ //   document.getElementById("text").textContent = "welcome to my website";
+//}
 
 
-function clearMessage() {
-    document.getElementById("text").textcontent = "";
-}
+//function clearMessage() {
+  //  document.getElementById("text").textcontent = "";
+//}
 
-document.getElementById("btnShow").addEventListener("click", showMessage);
-document.getElementById("btnClear").addEventListener("click", clearMessage);
+//document.getElementById("btnShow").addEventListener("click", showMessage);
+//document.getElementById("btnClear").addEventListener("click", clearMessage);
 
 // input
-document.getElementById("submitbtn").addEventListener("click", showName);
+//document.getElementById("submitbtn").addEventListener("click", showName);
 
-function showName() {
-    let name = document.getElementById("nameInput").value;
-    document.getElementById("output").textContent = "Hello " + name;
-} 
+//function showName() {
+  //  let name = document.getElementById("nameInput").value;
+    //document.getElementById("output").textContent = "Hello " + name;
+//} 
 
 
 // inputs and user data
 
-function greetUser() {
-    let name = document.getElementById("username").value;
-    document.getElementById("output").innerText = "Hello " + name;
-  }
+//function greetUser() {
+  //  let name = document.getElementById("username").value;
+    //document.getElementById("output").innerText = "Hello " + name;
+  //}
   
+  //let name = prompt("what is your name?");
 
-  
+  //alert(name);
+
+  //let name = prompt("what is your name?")
+  //let age = number(prompt("how old are you?"))
+
+  //alert("hello ", + name + "you are now " + age +"years old")
+
+  //hello//
+
+
+ // GLOBAL SCOPE //
+  let name = "praise";
+
+  function greet() {
+    console.log(name);
+  }
+
+  // FUNCTION SLOPE //
+  function test() {
+    let age = 18
+  }
+console.log(age)
+
+// BLOCK SCOPE //
+if (true) {
+    let city = "abuja"
+}
+console.log(city)
+
+// TASK //
+const title = "Prise"
+const age = 18
+
+function introduce() {
+    console.log(`my name is ${title} and i am ${18} years old`)
+}
+introduce()
+
+
+// FIX CODE //
+if (true) {
+    var message = "Hello"
+}
+console.log(message)
+
+// FIXED //
+let message;
+if (true) {
+    message ="Hello";
+}
+console.log(message)
+
+// PRACTICE //
+let favouritelanguage = "javascript"
+
+function introduce() {
+    console.log(`my favourite language is ${favouritelanguage} `)
+}
+introduce()
+
+// OR //
+
+let favoritelanguage;
+
+function introduce() {
+    favoritelanguage = "javascript"
+}
+console.log(`my favourite language is ${favoritelanguage} `)
